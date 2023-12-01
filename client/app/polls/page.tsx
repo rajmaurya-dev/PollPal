72bdcb9065f026e92110560bcfd47f70d948586c
Raj <blcc.raj@gmail.com>
2023-12-01 12:06:07 +0530

    feat: new poll

--- client/app/polls/page.tsx
+++ client/app/polls/page.tsx
@@ -2,6 +2,15 @@
 import UserHeader from '@/components/ui/userHeader';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import Poll from 'react-polls';
+const pollStyles1 = {
+    questionSeparator: true,
+    questionSeparatorWidth: "question",
+    questionBold: true,
+    questionColor: "#ffffff",
+    align: "center",
+    theme: "purple"
+};
 
 interface Option {
     _id: string;
@@ -44,6 +53,52 @@ const AllPolls: React.FC = () => {
         };
         fetchPolls();
     }, [])
+    const handleVoteChange = (
+        pollId: string,
+        optionId: string,
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const newPolls = polls.map((poll) => {
+            if (poll._id === pollId) {
+                const newOptions = poll.options.map((option) => {
+                    if (option._id === optionId) {
+                        return { ...option, votes: Number(event.target.value) };
+                    }
+                    return option;
+                });
+
+                return { ...poll, options: newOptions };
+            }
+            return poll;
+        });
+
+        setPolls(newPolls);
+    };
+    const handleVote = (vote: number, pollId: string) => {
+        // Implement your logic to update votes on the server
+        // Update the state or send a request to your API
+        console.log(`Voted for option ${vote} in poll ${pollId}`);
+    };
+    const customVote = async (vote: number, pollId: string) => {
+        try {
+
+            const response = await axios.post(
+                `${process.env.NEXT_PUBLIC_SERVER_PATH}/polls/${pollId}`,
+                { answer: vote },
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true,
+                }
+            );
+
+            // Optionally, you can handle the response here
+
+            console.log(`Voted for option ${vote} in poll ${pollId}`);
+        } catch (error) {
+            console.error('Error voting:', error);
+            // Handle the error if needed
+        }
+    };
 
     return (
         <div className="mx-2 md:mx-20 mt-8">
@@ -52,8 +107,8 @@ const AllPolls: React.FC = () => {
 
                 <UserHeader />
             </div>
-            <ul className='flex gap-3 items-center justify-around flex-wrap'>
-                {polls.map((poll) => (
+            <ul className='flex gap-3 justify-around  flex-wrap'>
+                {/* {polls.map((poll) => (
                     <li
                         key={poll._id}
                         className="bg-[#43547A] rounded-md shadow-md p-4 mb-4 w-[270px] h-[200px]"
@@ -80,8 +135,26 @@ const AllPolls: React.FC = () => {
                                     </li>
                                 ))}
                             </ul>
+
                         </div>
                     </li>
+                ))} */}
+                {polls.map((poll) => (
+                    <div className='min-w-[280px] min-h-[200px] rounded-md text-white bg-[#43547a6a]'>
+
+                        <Poll
+                            key={poll._id}
+                            question={poll.title}
+                            pollStyles={pollStyles1}
+                            questionBold={true}
+                            answers={poll.options.map((option) => ({
+                                option: option.option,
+                                votes: option.votes,
+                            }))}
+                            onVote={(vote) => customVote(vote, poll._id)}
+                            noStorage={true}
+                        />
+                    </div>
                 ))}
             </ul>
         </div>
