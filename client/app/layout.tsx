82085cf1b4088a89caa060e9d7e9d76b6f021cf3
Raj <blcc.raj@gmail.com>
2024-05-09 22:52:59 +0530

    style: Update metadata and OG image for PollPal app

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -10,8 +10,38 @@ const poppins = Poppins({ weight: ["400"], subsets: ['latin'] })
 
 
 export const metadata: Metadata = {
-  title: 'PollPal',
-  description: 'Fun Project',
+  title: 'PollPal |  An AI Powered Polling App',
+  description: 'Create polls with a tap, let AI do the rest. PollPal makes sharing opinions fun and effortless with your friend squad',
+  keywords: ['poll', 'polling', 'AI', 'opinion', 'friends', 'fun', 'share', 'create', 'vote', 'pollpal', 'poll pal'
+  ],
+  openGraph: {
+    url: "https://pollpal.vercel.app",
+    type: "website",
+    title: "PollPal |  An AI Powered Polling App",
+    description: "Create polls with a tap, let AI do the rest. PollPal makes sharing opinions fun and effortless with your friend squad",
+    images: [
+      {
+        url: "poll-pal.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "PollPal",
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "PollPal |  An AI Powered Polling App",
+    description: "Create polls with a tap, let AI do the rest. PollPal makes sharing opinions fun and effortless with your friend squad",
+    creator: "@rajmauryafr",
+    images: [
+      {
+        url: "poll-pal.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "PollPal",
+      }
+    ]
+  },
   icons: {
     icon: '/logo.png',
   },
